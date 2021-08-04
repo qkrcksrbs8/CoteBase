03ca66981525a34be294f376645f3abd819d50a3
ParkChanGyun <qkrcksrbs8@naver.com>
2021-08-04 21:50:43 +0900

    거듭제곱

---
+++ CoteBase/src/main/java/base/_19.java
@@ -0,0 +1,19 @@
+package base;
+
+public class _19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int m = 7;
+		int res = 1;
+		
+		for (int i = 0; i< m; ++i) {
+			res *= n;
+		}
+		
+		System.out.println(res);
+		
+	}
+
+}
